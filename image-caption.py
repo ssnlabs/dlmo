4869b452c4dc9b74c37d2324662605cf9e224877
ssnlabs <137744428+ssnlabs@users.noreply.github.com>
2024-11-17 23:00:23 +0530

    Update image-caption.py

--- image-caption.py
+++ image-caption.py
@@ -1,98 +1,155 @@
-import os
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.utils import pad_sequences
-from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, RepeatVector
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Model, Sequential
-import cv2
-import matplotlib.pyplot as pt
-#https://www.kaggle.com/datasets/adityajn105/flickr8k
-import os
-def load_data(image_folder, caption_file, limit=20):
-    imgs, image_paths, captions = [], [], []
-    unique_images = set()      
-    with open(caption_file, 'r') as file:
-        lines = file.readlines()
-    for line in lines:
-        img_name, caption = line.strip().split(',', 1)  
-        img_path = os.path.join(image_folder, img_name)
-        if len(unique_images) >= limit:
-            break
-        if img_name not in unique_images and os.path.exists(img_path):
-            unique_images.add(img_name)
-            imgs.append(img_name)
-            image_paths.append(img_path)
-            captions.append(caption)
-    return imgs, image_paths, captions
-
-image_folder = r"Images"
-caption_file = r"captions.txt"
-
-imgs, image_paths, captions = load_data(image_folder, caption_file, limit=500)
-
+import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
+import os
+import kagglehub
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
+import tensorflow as tf
 
-def display_images(image_paths, limit=20):
-    plt.figure(figsize=(10, 10))    
-    for i in range(min(limit, len(image_paths))):
-        image = Image.open(image_paths[i])
-        plt.subplot(5, 5, i + 1)  
-        plt.imshow(image)
-        plt.axis('off')
-    plt.show()
+path = kagglehub.dataset_download("adityajn105/flickr8k")
+caption_file = os.path.join(path, 'captions.txt')
+image_folder = os.path.join(path, 'Images')
 
-display_images(image_paths)
-image_paths = image_paths[1:12]
-captions = captions[1:12]
+def load_captions(caption_file, num_images=20):
+    df = pd.read_csv(caption_file)    
+    image_ids = df['image'].unique()[:num_images]
+    df = df[df['image'].isin(image_ids)]
+    
+    captions_dict = {}
+    for _, row in df.iterrows():
+        if row['image'] not in captions_dict:
+            captions_dict[row['image']] = []
+        caption = 'startseq ' + row['caption'].lower() + ' endseq'
+        captions_dict[row['image']].append(caption)
+    
+    return captions_dict
 
-cnn_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-cnn_model = Model(inputs=cnn_model.inputs, outputs=cnn_model.layers[-1].output)
+captions_dict = load_captions(caption_file)
 
-import matplotlib.pyplot as plt
+def load_images(image_folder, image_names):
+    model = VGG16()
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    
+    features = {}
+    for img_name in image_names:
+        img_path = os.path.join(image_folder, img_name)
+        img = load_img(img_path, target_size=(224, 224))
+        img = img_to_array(img)
+        img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+        img = tf.keras.applications.vgg16.preprocess_input(img)
+        
+        feature = model.predict(img, verbose=0)
+        features[img_name] = feature
+    
+    return features
 
-def extract_features(image):
-    image = np.expand_dims(image, axis=0)  
-    return cnn_model.predict(image)
+features = load_images(image_folder, list(captions_dict.keys()))
 
-def build_captioning_model(vocab_size, max_caption_length):
-    model = Sequential()
-    model.add(Embedding(input_dim=vocab_size, output_dim=256, input_length=max_caption_length))
-    model.add(LSTM(256, return_sequences=True))
-    model.add(Dense(vocab_size, activation='softmax'))
+def prepare_text_data(captions_dict):
+    all_captions = []
+    for img_captions in captions_dict.values():
+        all_captions.extend(img_captions)
+    
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(all_captions)
+    vocab_size = len(tokenizer.word_index) + 1
+    
+    max_length = max(len(caption.split()) for caption in all_captions)
+    
+    return tokenizer, vocab_size, max_length
+
+tokenizer, vocab_size, max_length = prepare_text_data(captions_dict)
+
+def prepare_training_data(captions_dict, features, tokenizer, max_length, vocab_size):
+    X1, X2, y = [], [], []
+    
+    for img_name, captions in captions_dict.items():
+        feature = features[img_name][0]
+        
+        for caption in captions:
+            seq = tokenizer.texts_to_sequences([caption])[0]
+            
+            for i in range(1, len(seq)):
+                in_seq = seq[:i]
+                out_seq = seq[i]
+                
+                in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                out_seq = tf.keras.utils.to_categorical([out_seq], num_classes=vocab_size)[0]
+                
+                X1.append(feature)
+                X2.append(in_seq)
+                y.append(out_seq)
+    
+    return np.array(X1), np.array(X2), np.array(y)
+
+X1, X2, y = prepare_training_data(captions_dict, features, tokenizer, max_length, vocab_size)
+
+def create_tf_dataset(X1, X2, y, batch_size=32):
+    dataset = tf.data.Dataset.from_tensor_slices(((X1, X2), y))
+    dataset = dataset.shuffle(1000).batch(batch_size)
+    return dataset
+
+dataset = create_tf_dataset(X1, X2, y)
+
+def create_model(vocab_size, max_length):    
+    inputs1 = Input(shape=(4096,))
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+    
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+    
+    decoder1 = add([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+    
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
     model.compile(loss='categorical_crossentropy', optimizer='adam')
+    
     return model
 
-vocab_size = 1000           
-max_caption_length = 10      
+model = create_model(vocab_size, max_length)
 
-captioning_model = build_captioning_model(vocab_size, max_caption_length)
-captioning_model.summary()
+model.summary()
 
-image = np.random.rand(224, 224, 3)  
-caption = np.random.randint(1, vocab_size, (1, max_caption_length))
+model.fit(dataset, epochs=20, verbose=1)
 
-image_features = extract_features(image)
-print("Extracted Image Features:", image_features.shape)
+def generate_caption(model, tokenizer, feature, max_length):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = model.predict([feature, sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = ''
+        for w, index in tokenizer.word_index.items():
+            if index == yhat:
+                word = w
+                break
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'endseq':
+            break    
+    caption = in_text.replace('startseq', '').replace('endseq', '').strip()
+    return caption
 
-captioning_model.fit(caption, np.random.rand(1, max_caption_length, vocab_size), epochs=10,verbose=1)
+test_image = list(features.keys())[7]
+test_feature = features[test_image]
+caption = generate_caption(model, tokenizer, test_feature, max_length)
 
-reference_captions = [
-    ["a", "sample", "caption", "of", "an", "image"],
-    ["another", "description", "of", "the", "image", "content"]
-]
+img_path = os.path.join(image_folder, test_image)
+img = Image.open(img_path)
+plt.imshow(img)
+plt.axis('off')
+plt.show()
 
-!pip install nltk
-
-from nltk.translate.bleu_score import sentence_bleu
-
-def evaluate_bleu(reference, candidate):
-    reference = [reference]  
-    return sentence_bleu(reference, candidate)
-
-dummy_generated_caption = ["this", "is", "a", "generated", "caption"]
-bleu_score = evaluate_bleu(reference_captions[0], dummy_generated_caption)
-print("BLEU score for the generated caption:", bleu_score)
+print(f"Generated caption for {test_image}: {caption}")
